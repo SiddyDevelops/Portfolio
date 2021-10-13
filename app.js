8f9bf3c1f6e9537d2bcfe3c608a3825b3e2f474c
omrsfylmz <omrsfylmz@gmail.com>
2021-10-13 09:40:28 +0300

    delece log

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 const links = Array.from(document.getElementsByClassName("nav-link"));
 
-// const highlight = (el) => {
-//   console.log(el);
-// };
-
 links.forEach((el) => {
   el.addEventListener("click", () => {
     el.classList.toggle("selected");
